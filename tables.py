2ca44a986c249e2f327e23cbd0a90daa800e48ab
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2023-03-04 21:03:55 -0700

    created databases, inserting values to tables

---
+++ tables.py
@@ -0,0 +1,78 @@
+# Uninformed
+# Self-Optimized
+# User-Optimized
+
+import sqlite3
+import csv
+import random
+
+
+def main():
+    createSmallDB()
+
+def insertRandomCSV(filename, size):
+    with open(filename, 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        rows = []
+        for row in csv_reader:
+            rows.append({'customer_id': row['customer_id'], 'customer_postal_code': row['customer_zip_code_prefix']})
+        random.shuffle(rows)
+        rows = rows[:size]
+    return rows
+def createSmallDB():
+    conn = sqlite3.connect("A3Small.db")
+    c = conn.cursor()
+
+    customerData = insertRandomCSV("olist_customers_dataset.csv", 10000)
+    column_names = list(customerData[0].keys())
+
+    c.execute(""" 
+    CREATE TABLE "Customers" (
+    "customer_id" TEXT, 
+    "customer_postal_code" INTEGER,
+    PRIMARY KEY("customer_id")
+    );
+    """
+    )
+    c.execute(""" 
+    CREATE TABLE Sellers (
+    "seller_id" TEXT,
+    "seller_postal_code" INTEGER,
+    PRIMARY KEY("seller_id")
+    )
+    """
+    )
+    c.execute(""" 
+    CREATE TABLE Orders (
+    "order_id" TEXT,
+    "customer_id" TEXT, 
+    PRIMARY KEY("order_id"),
+    FOREIGN KEY("customer_id") REFERENCES "Customers"("customer_id")
+    )
+    """
+    )
+    c.execute(""" 
+    CREATE TABLE Order_items (
+    "order_id" TEXT,
+    "order_item_id" INTEGER, 
+    "product_id" TEXT,
+    "seller_id" TEXT,
+    PRIMARY KEY("order_id","order_item_id","product_id","seller_id"),
+    FOREIGN KEY("seller_id") REFERENCES "Sellers"("seller_id")
+    FOREIGN KEY("order_id") REFERENCES "Orders"("order_id")
+    )
+    """
+    )
+    placeholders = ', '.join(['?' for _ in column_names])
+    print(column_names)
+    insert_query = f'INSERT INTO Customers ({", ".join(column_names)}) VALUES ({placeholders})'
+    c.executemany(insert_query, [tuple(row[column_name] for column_name in column_names) for row in customerData])
+
+    conn.commit()
+    conn.close()
+
+
+
+main()
+
+
