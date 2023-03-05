dbead5bcf662399b4402ec454c32dde73c0b83d7
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2023-03-04 21:23:36 -0700

    new inserting functions

--- tables.py
+++ tables.py
@@ -10,20 +10,56 @@ import random
 def main():
     createSmallDB()
 
-def insertRandomCSV(filename, size):
+def insertRandomCustomer(filename, size):
+
     with open(filename, 'r') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        rows = []
-        for row in csv_reader:
-            rows.append({'customer_id': row['customer_id'], 'customer_postal_code': row['customer_zip_code_prefix']})
-        random.shuffle(rows)
-        rows = rows[:size]
+            csv_reader = csv.DictReader(csvfile)
+            rows = []
+            for row in csv_reader:
+                rows.append({'customer_id': row['customer_id'], 'customer_postal_code': row['customer_zip_code_prefix']})
+            random.shuffle(rows)
+            rows = rows[:size]
+
+        
     return rows
+
+def insertRandomSeller(filename, size):
+    with open(filename, 'r') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            rows = []
+            for row in csv_reader:
+                rows.append({'seller_id': row['seller_id'], 'seller_postal_code': row['seller_zip_code_prefix']})
+            random.shuffle(rows)
+            rows = rows[:size]
+    return rows
+
+def insertRandomOrder(filename, size):
+        with open(filename, 'r') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            rows = []
+            for row in csv_reader:
+                rows.append({'order_id': row['order_id'], 'customer_id': row['customer_id']})
+            random.shuffle(rows)
+            rows = rows[:size]
+
+
+def insertRandomOrderItem(filename, size):
+    with open(filename, 'r') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            rows = []
+            for row in csv_reader:
+                rows.append({'order_id': row['order_id'], 'order_item_id': row['order_item_id'], 'product_id': row['product_id'], 'seller_id': row['seller_id']})
+            random.shuffle(rows)
+            rows = rows[:size]
+    return rows
+
+
+
 def createSmallDB():
     conn = sqlite3.connect("A3Small.db")
     c = conn.cursor()
 
-    customerData = insertRandomCSV("olist_customers_dataset.csv", 10000)
+    customerData = insertRandomCustomer("olist_customers_dataset.csv", 10000)
     column_names = list(customerData[0].keys())
 
     c.execute(""" 
@@ -64,12 +100,23 @@ def createSmallDB():
     """
     )
     placeholders = ', '.join(['?' for _ in column_names])
-    print(column_names)
     insert_query = f'INSERT INTO Customers ({", ".join(column_names)}) VALUES ({placeholders})'
     c.executemany(insert_query, [tuple(row[column_name] for column_name in column_names) for row in customerData])
 
+
+
+
+
+    
+
     conn.commit()
     conn.close()
+
+
+
+
+
+
 
 
 
